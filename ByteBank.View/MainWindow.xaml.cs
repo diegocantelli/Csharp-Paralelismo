4083f7f26bcb99e71b0bb69d3b80fcc3c27f1691
Diego Cantelli <diegocantelli19@gmail.com>
2021-04-02 14:30:43 -0300

    Obtendo o retorno da Task através de Task.Result

--- ByteBank.View/MainWindow.xaml.cs
+++ ByteBank.View/MainWindow.xaml.cs
@@ -42,49 +42,42 @@ namespace ByteBank.View
 
             var contas = r_Repositorio.GetContaClientes();
 
-            var resultado = new List<string>();
-
             AtualizarView(new List<string>(), TimeSpan.Zero);
 
             var inicio = DateTime.Now;
 
-            //Para cada conta na lista será criada uma nova Task
-            var contasTarefas = contas.Select(conta =>
-            {
-                //Task.Factory.StartNew -> Abstrai a necessidade da criação de threads, a própria função
-                // se ocupa de verificar o processamento e os núcleos disponíveis ou menos sobrecarregados para
-                // executar o código
-                return Task.Factory.StartNew(() =>
-                {
-                    //código que será executado na task
-                    var resultadoConta = r_Servico.ConsolidarMovimentacao(conta);
-                    resultado.Add(resultadoConta);
-                });
-                //To Array executa o código 
-            }).ToArray();
-
-
-            //Cria uma tarefa que será concluída quando todas as tarefas dentro do array forem finalizadas.
-            // Ao contrário de WaitAll, WhenAll não bloqueia a thread principal, neste caso a thread da UI
-            Task.WhenAll(contasTarefas)
-                // O código dentro de ContinueWith só será executado quando a Task anterior for finalizada,
-                // no caso todas as tarefas dentro de Task.WhenAll(contasTarefas)
-                // task ->  Task.WhenAll(contasTarefas)
+            ConsolidarContas(contas)
                 .ContinueWith(task =>
                 {
                     var fim = DateTime.Now;
-
-                    //Atualizar a view só faz sentido quando todas as tasks forem finalizadas
+                    //task.Result -> Irá retornar a lista de strings, que é o retorno desta task
+                    var resultado = task.Result;
                     AtualizarView(resultado, fim - inicio);
-                    //taskSchedulerUI -> indica que o código dentro de ContinueWith deverá ser executado no
-                    // contexto da Thread principal
                 }, taskSchedulerUI)
                 .ContinueWith(task =>
                 {
                     BtnProcessar.IsEnabled = true;
-                }, taskSchedulerUI);
-            
-           
+                });
+        }
+
+        private Task<List<String>> ConsolidarContas(IEnumerable<ContaCliente> contas)
+        {
+            var resultado = new List<string>();
+
+            var tasks = contas.Select(conta =>
+            {
+                return Task.Factory.StartNew(() =>
+                {
+                    var contaResultado = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(contaResultado);
+                });
+            });
+
+            //Retornando uma Tarefa que também possui retorno
+            return Task.WhenAll(tasks).ContinueWith(t => {
+                return resultado;
+            });
+
         }
 
         private void AtualizarView(List<String> result, TimeSpan elapsedTime)
